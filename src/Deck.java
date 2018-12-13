ed115c0b289aebafff90e1714a279028203524f2
BT_1N3_09 <d>
2018-12-13 10:12:26 -0500

    PLZ PUSH

--- src/Deck.java
+++ src/Deck.java
@@ -1,12 +1,16 @@
 import java.util.ArrayList;
 
 public class Deck {
-    private ArrayList<Card> unDealt;
-    private ArrayList<Card> dealt;
+    public ArrayList<Card> unDealt;
+    public ArrayList<Card> dealt;
 
     public Deck( String[] ranks, String[] suits, int[] points)
     {
-        for (int i=0; i<5; i++)
+        ArrayList<Card>unDealt= new ArrayList<Card>();
+        ArrayList<Card>dealt= new ArrayList<Card>();
+      /*  this.unDealt=unDealt;
+        this.dealt=dealt;*/
+        for (int i=0; i<51; i++)
         {
 
             Card sample= new Card (ranks[(int)(Math.random()*13)], suits[(int)(Math.random()*4)],points[(int)(Math.random()*10)]);
@@ -14,7 +18,7 @@ public class Deck {
             System.out.println(unDealt.get(i).toString());
         }
 
-        this.unDealt=unDealt;
+
     }
 
     public void isitfull()
@@ -22,8 +26,42 @@ public class Deck {
         for (int i=0; i< this.unDealt.size(); i++)
             System.out.println(this.unDealt.get(i));
     }
-    private boolean isEmpty()
+    public boolean isEmpty()
     {
+        if (this.unDealt.size()!=0)
+        {
+            return true;
+        }
         return false;
+
     }
+    public int size()
+    {
+        return this.unDealt.size();
+    }
+    public Card deal()
+    {
+        if (this.unDealt.size()==0)
+        {
+            return null;
+        }
+        Card remCard= this.unDealt.get(0);
+        this.dealt.add(0,remCard);
+        this.unDealt.remove(0);
+        return remCard;
+    }
+
+    public void shuffle()
+    {
+        for (int k=51; k>1; k--)
+        {
+            Card tempCard= this.dealt.get(0);
+            this.unDealt.add(tempCard);
+            this.dealt.remove(0);
+        }
+    }
+
+
+
 }
+
--- src/DeckTester.java
+++ src/DeckTester.java
@@ -1,13 +1,57 @@
 import java.util.ArrayList;
 
 public class DeckTester {
-    public static void main(String[] args)
-    {
-        ArrayList<Card> unDealt= new ArrayList<Card>();
-        String[] ranks= {"Jack","King","Queen","Ace","2","3","4","5","6","7","8","9","10"};
-        String[] suits= {"Hearts ","Clubs ","Spades ","Diamonds "};
-        int[] points={1,2,3,4,5,6,7,8,9,10};
+    public static void main(String[] args) {
+        ArrayList<Card> unDealt = new ArrayList<Card>();
+        ArrayList<Card> p1 = new ArrayList<Card>();
+        ArrayList<Card> p2 = new ArrayList<Card>();
 
-        Deck deck= new Deck(ranks, suits, points);
-        
+        String[] ranks = {"Jack", "King", "Queen", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10"};
+        String[] suits = {"Hearts ", "Clubs ", "Spades ", "Diamonds "};
+        int[] points = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+        Deck deck = new Deck(ranks, suits, points);
+
+
+        for (int i = 0; i < 51; i++) {
+            System.out.println(deck.unDealt.get(i).toString());
+        }
+
+        for (int i = 0; i < 51; i++) {
+            System.out.println(p1.get(i).toString());
+        }
+
+        for (int i = 0; i < 51; i++) {
+            System.out.println(p2.get(i).toString());
+        }
+
+
+
+        System.out.println();
+       // deck.unDealt.isitfull();
+        // p1.isitfull();
+        // p2.isitfull();
+
+        System.out.println();
+        System.out.println(deck.unDealt.isEmpty());
+        System.out.println(p1.isEmpty());
+        System.out.println(p2.isEmpty());
+
+        System.out.println();
+        System.out.println(deck.unDealt.size());
+        System.out.println(p1.size());
+        System.out.println(p2.size());
+
+        System.out.println();
+        for (int i=0; i<51; i++)
+        {
+            System.out.println(deck.deal());
+        }
+
+
+        System.out.println();
+        deck.shuffle();
+
+    }
 }
+
